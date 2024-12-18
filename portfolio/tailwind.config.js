d79ddb74aa0d4b584eb2330571358b69fa9cb92d
Dark Coder <codewithdark90@gmail.com>
2024-12-18 19:36:42 +0500

    -- add

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./portfolio/**/*.{js,ts,jsx,tsx}", // Adjust the paths as necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
